9198f32fcd1d821fea4fdef88db67fd39efc4854
AnoshF1 <anoshfernando751@gmail.com>
2022-06-07 19:30:47 +0530

    lendeds/transactions/proucts completed

---
+++ app/Http/Controllers/API/LendedController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreLendedRequest;
+use App\Models\Lended;
+use Illuminate\Http\Request;
+
+class LendedController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $lended = Lended::all();
+        return response()->json([
+            'status' => true,
+            'posts' => $lended
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreLendedRequest $request)
+    {
+        $lended = Lended::create($request->all());
+        return response()->json([
+            'status' => true,
+            'message' => "Lending was Successfully",
+            'post' => $lended
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Lended  $lended
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lended $lended)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Lended  $lended
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Lended $lended)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Lended  $lended
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreLendedRequest $request, Lended $lended)
+    {
+        $lended->update($request->all());
+        return response()->json([
+            'status' => true,
+            'message' => "lending details updated Successfully!",
+            'post' => $lended
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Lended  $lended
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lended $lended)
+    {
+        $lended->delete();
+        return response()->json([
+            'status' => true,
+            'message' => "Lend declined Successfully!"
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,18 @@
 <?php
 
-use App\Http\Controllers\API\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\LoginController;
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\LendedController;
+use App\Http\Controllers\API\TransactionController;
 
+/* Used for the Login and Registration of the Admin*/
 Route::apiResource('register', RegisterController::class);
 Route::apiResource('login', LoginController::class);
+/* CRUDS */
+Route::apiResource('lendeds', LendedController::class);
+Route::apiResource('transactions', TransactionController::class);
 // Route::controller(LoginController::class)->group(function () {
 //     Route::post('login', 'login');
 //     Route::post('register', 'register');
@@ -20,10 +26,7 @@ Route::apiResource('login', LoginController::class);
 //     Route::post('logout', 'logout');
 //     Route::post('refresh', 'refresh');
 //     Route::get('me', 'me');
-
 // });
-// Route::post('register', 'API\RegisterController@register');
-// Route::post('login', 'API\RegisterController@login');
 // Route::middleware('auth:api')->group( function () {
 //     Route::resource('products', 'API\ProductController');
 // });
